e5d3169cced86347d68ff9e0d06e469438712d5e
Philippine Lorrain <philippinelorrain@gmail.com>
2022-10-21 16:00:12 -0400

    setting state so that it clears inputs after typing

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -10,7 +10,9 @@ function AddUser(props) {
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    console.log(enteredUsername, enteredAge);
+    // console.log(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
   };
 
   const usernameChangeHandler = (event) => {
@@ -25,9 +27,15 @@ function AddUser(props) {
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>
-        <input id='username' type='text' onChange={usernameChangeHandler} />
+        <input id='username' type='text'
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+          />
         <label htmlFor='age'>Age</label>
-        <input id='age' type='number' onChange={ageChangeHandler} />
+        <input id='age' type='number'
+          value={enteredAge}
+          onChange={ageChangeHandler}
+          />
         <Button type='submit'>Add User</Button>
       </form>
     </Card>
