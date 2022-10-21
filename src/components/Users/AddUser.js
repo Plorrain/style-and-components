64a10a00c59e28f24a36708a6e7f982266915c29
Philippine Lorrain <philippinelorrain@gmail.com>
2022-10-21 14:58:39 -0400

    installing new app to follow course : building a form with alerts

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AddUser() {
+   return (
+    <form>
+      <label htmlFor='username'>Username</label>
+      <input id='username' type='text' />
+    </form>
+   )
+}
+
+export default AddUser;
