858a39f18aa874c80779b0b36d29605abaab12b3
Philippine Lorrain <philippinelorrain@gmail.com>
2022-10-21 16:37:00 -0400

    linking css file to JS and linking the card component

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import Card from '../UI/Card';
+
 import classes from './UsersList.module.css';
 
 function UsersList(props) {
 return (
-  <ul>
-    <li>
-      {props.users}
-    </li>
-  </ul>
-)
-}
+  <Card className={classes.users}>
+      <ul>
+          {props.users.map((user) => (
+          <li>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+};
 
 export default UsersList;
