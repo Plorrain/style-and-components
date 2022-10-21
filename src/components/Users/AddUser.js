cf6320a66ddcb8887bf36deecb4d061c7f70830a
Philippine Lorrain <philippinelorrain@gmail.com>
2022-10-21 15:17:08 -0400

    creating a crad component to wrap the form in

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import '../UI/Card';
 
-function AddUser() {
+function AddUser(props) {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
    return (
-    <form>
-      <label htmlFor='username'>Username</label>
-      <input id='username' type='text' />
-    </form>
+    <Card>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor='username'>Username</label>
+        <input id='username' type='text' />
+        <label htmlFor='age'>Age</label>
+        <input id='age' type='number' />
+        <button type='submit'>Add User</button>
+      </form>
+    </Card>
    )
 }
 
