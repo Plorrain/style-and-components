f4784ac7b92998ad2f4772ab7935f3d768ecc4ba
Philippine Lorrain <philippinelorrain@gmail.com>
2022-10-21 17:36:11 -0400

    errormodal not showing in browser but should because it is not dynamic

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -32,25 +32,25 @@ function AddUser(props) {
   };
 
    return (
-    <div>
-      <ErrorModal title='An error occured!' message='Something went wrong' />
-        <Card className={classes.input}>
-          <form onSubmit={addUserHandler}>
-            <label htmlFor='username'>Username</label>
-            <input id='username' type='text'
-              value={enteredUsername}
-              onChange={usernameChangeHandler}
-              />
-            <label htmlFor='age'>Age</label>
-            <input id='age' type='number'
-              value={enteredAge}
-              onChange={ageChangeHandler}
-              />
-            <Button type='submit'>Add User</Button>
-          </form>
-        </Card>
-    </div>
-   )
+  <div>
+    <ErrorModal title='An error occured!' message='Something went wrong.' />
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor='username'>Username</label>
+          <input id='username' type='text'
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+            />
+          <label htmlFor='age'>Age</label>
+          <input id='age' type='number'
+            value={enteredAge}
+            onChange={ageChangeHandler}
+            />
+          <Button type='submit'>Add User</Button>
+        </form>
+      </Card>
+  </div>
+  )
 }
 
 export default AddUser;
