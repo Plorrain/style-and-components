dc6b68c72f828eb990df299ef798dd94e6a0c616
Philippine Lorrain <philippinelorrain@gmail.com>
2022-10-24 13:51:55 -0400

    few corrections. Still looking for why the error modal isn't showing (no errors in the console either

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -14,10 +14,10 @@ function AddUser(props) {
     // console.log(enteredUsername, enteredAge);
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       return;
-    }
+    };
     if (+enteredAge < 1) {
       return;
-    }
+    };
     props.onAddUser(enteredUsername, enteredAge);
     setEnteredUsername('');
     setEnteredAge('');
@@ -34,21 +34,21 @@ function AddUser(props) {
   return (
     <div>
       <ErrorModal title='An error occured!' message='Something went wrong.' />
-        <Card className={classes.input}>
-          <form onSubmit={addUserHandler}>
-            <label htmlFor='username'>Username</label>
-            <input id='username' type='text'
-              value={enteredUsername}
-              onChange={usernameChangeHandler}
-              />
-            <label htmlFor='age'>Age</label>
-            <input id='age' type='number'
-              value={enteredAge}
-              onChange={ageChangeHandler}
-              />
-            <Button type='submit'>Add User</Button>
-          </form>
-        </Card>
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor='username'>Username</label>
+          <input id='username' type='text'
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+            />
+          <label htmlFor='age'>Age</label>
+          <input id='age' type='number'
+            value={enteredAge}
+            onChange={ageChangeHandler}
+            />
+          <Button type='submit'>Add User</Button>
+        </form>
+      </Card>
     </div>
     )
   }
