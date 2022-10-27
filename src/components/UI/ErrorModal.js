7dd8863e3af2a7cf01f19778e9f4bfe263851d86
Philippine Lorrain <philippinelorrain@gmail.com>
2022-10-27 15:55:53 -0400

    had help figuring why it was not showing in browser: missing a 'return' in errorModal

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -5,20 +5,23 @@ import Button from './Button';
 import classes from './ErrorModal.module.css';
 
 function ErrorModal(props) {
-  <div>
-    <div className={classes.backdrop} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button>Okay</Button>
-        </footer>
-      </Card>
-  </div>
+  return (
+    <div>
+      <div className={classes.backdrop}>
+        <Card className={classes.modal}>
+          <header className={classes.header}>
+            <h2>{props.title}</h2>
+          </header>
+          <div className={classes.content}>
+            <p>{props.message}</p>
+          </div>
+          <footer className={classes.actions}>
+            <Button>Okay</Button>
+          </footer>
+        </Card>
+      </div>
+    </div>
+  );
 }
 
 export default ErrorModal;
